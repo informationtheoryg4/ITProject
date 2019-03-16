a8f1a61c56bae2546b61f491b19a0869f361cfe4
Giova <Giova@DESKTOP-JBVO785>
2019-03-16 12:33:26 +0100

    no

--- InformationTheoryProject/src/channels/Channel.java
+++ InformationTheoryProject/src/channels/Channel.java
@@ -4,7 +4,7 @@ package channels;
 public interface Channel {
 	
 	public void send();
-	public void receive(); //dhdhdhddh
+	public void receive(); //wy ar u gaie
 	public void getStatistics(); //TODO
 	
 }
