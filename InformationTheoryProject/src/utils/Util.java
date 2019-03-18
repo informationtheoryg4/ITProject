a3995fa6ea869d318e8ad1c682ba66d7e210e97f
Giova <Giova@DESKTOP-JBVO785>
2019-03-18 15:46:17 +0100

    Implementati metodi: imageToByteArray e byteArraytoImage

--- InformationTheoryProject/src/utils/Util.java
+++ InformationTheoryProject/src/utils/Util.java
@@ -1,6 +1,8 @@
 package utils;
 
+
 import java.awt.image.BufferedImage;
+import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileWriter;
@@ -11,6 +13,7 @@ import java.nio.file.Paths;
 import java.util.Arrays;
 
 import javax.imageio.ImageIO;
+import javax.swing.text.html.ImageView;
 //Classe con metodi statici di utilita'
 public class Util {
 	
@@ -57,11 +60,56 @@ public class Util {
               System.out.println("Error during reading/writing");
          }
       
+        byte [] b=sb.toString().getBytes();
         return sb.toString();
+       
 	}
+	
+	public static byte[] imageToByteArray(String filePath) {
+		 BufferedImage bImage;
+		 ByteArrayOutputStream bos=null;
+		try {
+			bImage = ImageIO.read(new File(filePath));
+			bos = new ByteArrayOutputStream();
+		      ImageIO.write(bImage, "jpg", bos );
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	      byte [] data = bos.toByteArray();
+	      return data;
+	}
+	
+	public static BufferedImage byteArrayToImage(byte []input) {
+		 ByteArrayInputStream bis = new ByteArrayInputStream(input);
+	      BufferedImage bImage2=null;
+		try {
+			bImage2 = ImageIO.read(bis);
+			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	      System.out.println("image created");
+	      return bImage2;
+	}
+	
+	/*
+	 * da implementare metodo per passare da file di testo ad array di byte
+	 */
 
 	public static void main(String[] args) {
-		imageToBinary("luna.jpg");
+		byte [] bytes=imageToByteArray("luna.jpg");
+		System.out.println("Array di bytes creato:\n");
+		System.out.println(bytes[3]);
+		BufferedImage bi= byteArrayToImage(bytes);
+		try {
+			ImageIO.write(bi, "jpg", new File("output.jpg") );
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
 	}
 
 }
